25835fa54f5f9d70366474c9ea7fa4259e0be47c
Kanika Jadhav <kanikajadhav.2003@gmail.com>
2026-03-21 00:24:44 +0530

    Hide database password using environment variable

--- src/main/java/com/library/db/DBConnection.java
+++ src/main/java/com/library/db/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/library_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "Kanika@0987"; 
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
